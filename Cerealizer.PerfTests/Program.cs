704a6f1212f4e1b1b9535be33b6147bc2d82f73d
Allan Ritchie <allan.ritchie@gmail.com>
2016-08-29 11:15:58 -0400

    perf tests

---
+++ Cerealizer.PerfTests/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Diagnostics;
+using Cerealizer.Attributes;
+using Cerealizer.Tests;
+
+
+namespace Cerealizer.PerfTests
+{
+    class Program
+    {
+        const int runs = 1000;
+
+
+        static void Main(string[] args)
+        {
+            var packet = "4c-00-02-15-9d-d3-c2-d7-c0-5e-41-74-92-d2-cc-30-62-9e-52-27-00-01-00-0a".FromHex();
+            Run("Cerealizer Tests", () => RunCerealizer(runs, packet));
+            Run("Raw Tests", () => RunRaw(runs, packet));
+            Console.WriteLine("Test Complete.  Press <ENTER> to quit");
+            Console.ReadLine();
+        }
+
+
+        static void Run(string category, Action action)
+        {
+            Console.WriteLine("Starting " + category);
+            var sw = new Stopwatch();
+            sw.Start();
+            action();
+            sw.Stop();
+            Console.WriteLine($"{category} finished in {sw.Elapsed}");
+        }
+
+
+        static void RunCerealizer(int count, byte[] packet)
+        {
+            var cerealizer = new AttributeCerealizer();
+            for (var i = 0; i < count; i++)
+            {
+                var b = cerealizer.Deserialize<Beacon>(packet);
+            }
+        }
+
+
+        static void RunRaw(int count, byte[] packet)
+        {
+            for (var i = 0; i < count; i++)
+            {
+                var uuid =  EndianBitConverter
+                    .ToString(packet, 4, 16, true)
+                    .Replace("-", String.Empty);
+
+                var b = new Beacon
+                {
+                    Uuid = new Guid(uuid),
+                    Major = EndianBitConverter.ToUInt16(packet, 20, false),
+                    Minor = EndianBitConverter.ToUInt16(packet, 22, false)
+                };
+            }
+        }
+    }
+}
--- Cerealizer.Tests/AttributeCerealizerTests.cs
+++ Cerealizer.Tests/AttributeCerealizerTests.cs
@@ -10,10 +10,10 @@ namespace Cerealizer.Tests
     [TestFixture]
     public class AttributeBufferParserTests
     {
-        [Test]
-        public void Deserialize_Success_Test()
+        [TestCase("91-5c-68-09-d0-66-f8-ee-94-01-00-00-82-8f-fa-56-0f-02-00-00")]
+        public void Deserialize_Success_Test(string hex)
         {
-            var bytes = StringToByteArray("91-5c-68-09-d0-66-f8-ee-94-01-00-00-82-8f-fa-56-0f-02-00-00");
+            var bytes = hex.FromHex();
             var serializer = new AttributeCerealizer();
             var obj = serializer.Deserialize<AttributeModel>(bytes);
 
@@ -24,32 +24,16 @@ namespace Cerealizer.Tests
         }
 
 
-        [Test]
-        public void Serializer_Success()
+        [TestCase("91-5c-68-09-d0-66-f8-ee-94-01-00-00-82-8f-fa-56-0f-02-00-00")]
+        public void Serializer_Success(string hex)
         {
-            var hex = "91-5c-68-09-d0-66-f8-ee-94-01-00-00-82-8f-fa-56-0f-02-00-00";
-            var bytes = StringToByteArray(hex);
-
+            var bytes = hex.FromHex();
             var serializer = new AttributeCerealizer();
             var obj = serializer.Deserialize<AttributeModel>(bytes);
             var bytes2 = serializer.Serialize(obj); // TODO: if I don't serialize all original bytes, this will fail obviously
             var hex2 = BitConverter.ToString(bytes2);
 
             hex.Should().BeEquivalentTo(hex2);
-        }
-
-
-        static byte[] StringToByteArray(string hex)
-        {
-            hex = hex
-                .Replace("-", String.Empty)
-                .Replace(" ", String.Empty);
-
-            return Enumerable
-                .Range(0, hex.Length)
-                .Where(x => x % 2 == 0)
-                .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
-                .ToArray();
         }
     }
 }
